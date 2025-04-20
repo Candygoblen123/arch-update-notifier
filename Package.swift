2c876573f795bb3e9eb720eb5087bc9e51d399bc
Andrew Glaze <andrew@candy123.moe>
2025-04-19 21:57:50 -0400

    only kill newly spawned faked

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .executableTarget(
             name: "arch-update-notifier",
             dependencies: [
-                .target(name: "Gio")
+                .target(name: "Gio"),
             ]),
         .systemLibrary(name: "Gio", pkgConfig: "gio-2.0"),
     ]
